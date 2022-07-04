6fc956e3adb41dd4361aa71fe28aa51b2b05bc88
Jason <git-admin@casjaysdev.com>
2022-07-04 16:59:14 -0400

    🗃️  modified: build.sh 🗃️

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@
 # @License       : LICENSE.md
 # @ReadME        : build.sh --help
 # @Copyright     : Copyright: (c) 2022 Jason Hempstead, Casjays Developments
-# @Created       : Monday, Jul 04, 2022 11:51 EDT
+# @Created       : Monday, Jul 04, 2022 16:55 EDT
 # @File          : build.sh
-# @Description   :
+# @Description   : Installer script for jgmenu
 # @TODO          :
 # @Other         :
 # @Resource      :
@@ -21,22 +21,100 @@ SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
-trap 'exitCode=${exitCode:-$?};[ -n "$BUILD_SH_TEMP_FILE" ] && [ -f "$BUILD_SH_TEMP_FILE" ] && rm -Rf "$BUILD_SH_TEMP_FILE" &>/dev/null' EXIT
-
+trap 'exitCode=${exitCode:-$?};[ -n "$BUILD_SH_TEMP_FILE" ] && [ -f "$BUILD_SH_TEMP_FILE" ] && rm -Rf "$BUILD_SH_TEMP_FILE" &>/dev/null;exit ${exitCode:-$?}' EXIT
+set -Eo pipefail
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup build function
+__make_build() {
+  local exitCode=1
+  local exitCode_make=0
+  local exitCode_cmake=0
+  local exitCode_configure=0
+  if [[ -f "configure" ]]; then
+    printf_color "$GREEN" "running configure"
+    ./configure \
+      --prefix="$BUILD_DESTDIR" \
+      --with-lx \
+      --with-pmenu 2>&1 |
+      tee -a "$BUILD_LOG_FILE" &>/dev/null
+    exitCode_configure=$?
+  fi
+  if [[ -f "Makefile" ]]; then
+    printf_color "$GREEN" "running make"
+    make clean 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      make -j$(nproc) 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      sudo make install DESTDIR="$BUILD_DESTDIR" 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+    exitCode_make=$?
+  fi
+  if [[ -f "CMakeLists.txt" ]]; then
+    mkdir build && cd build || exit 10
+    cmake .. 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      make -j$(nproc) 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      sudo make install 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      exitCode_cmake=$?
+  fi
+  [[ $exitCode_configure = 0 ]] &&
+    [[ $exitCode_make = 0 ]] &&
+    [[ $exitCode_cmake = 0 ]] &&
+    exitCode=0
+  return "${exitCode:-$?}"
+}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
-# Set functions
-__help() { printf_color "$BLUE" "Usage: $APPNAME []"; }
+# Set main functions
+__run_git() {
+  if [[ -d ".git" ]]; then
+    printf_color "$CYAN" "Updating the git repo"
+    git reset --hard && git pull 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+    if [[ $? = 0 ]]; then
+      return 0
+    else
+      printf_color "$RED" "Failed to clone from $BUILD_SRC_URL"
+      exit 1
+    fi
+  elif [[ -n "$BUILD_SRC_URL" ]]; then
+    printf_color "$CYAN" "Cloning the git repo"
+    git clone "$BUILD_SRC_URL" "$BUILD_SRC_DIR" 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+    if [[ $? = 0 ]]; then
+      return 0
+    else
+      printf_color "$RED" "Failed to clone from $BUILD_SRC_URL"
+      exit 1
+    fi
+  fi
+}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-__list_options() { echo "${1:-$ARRAY}" | sed 's|:||g;s|'$2'| '$3'|g' 2>/dev/null; }
+__check_log() {
+  local exitCode="$?"
+  if [[ -f "$BUILD_LOG_FILE" ]]; then
+    errors="$(grep 'fatal error' "$BUILD_LOG_FILE" || echo '')"
+    if [[ -n "$errors" ]] || [[ "$exitCode" -ne 0 ]]; then
+      printf_color "$RED" "The following errors have occurred:"
+      echo -e "$errors"
+      printf_color "$YELLOW" "Log file saved to $BUILD_LOG_FILE"
+      exitCode=1
+    else
+      rm -Rf "$BUILD_LOG_FILE" &>/dev/null
+      printf_color "$GREEN" "Build of $BUILD_NAME has completed without error"
+      exitCode=0
+    fi
+  fi
+  return "${exitCode:-$?}"
+}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Additional functions
 printf_color() { echo -e "\t\t${1:-}${2:-}${NC}"; }
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__help() {
+  [[ -z "$ARRAY" ]] || local array="[${ARRAY//,/ }]"
+  [[ -z "$LONGOPTS" ]] || local opts="[--${LONGOPTS//,/ --}]"
+  printf_color "$BLUE" "Usage: $APPNAME $opts $array"
+}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Check for needed applications
-type -P bash make &>/dev/null || { echo "Missing: bash" && exit 1; }
+type -P bash &>/dev/null || { printf_color "$RED" "Missing: bash" && exit 1; }
+type -P make &>/dev/null || { printf_color "$RED" "Missing: make" && exit 1; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set variables
+exitCode=10
 NC="$(tput sgr0 2>/dev/null)"
 RESET="$(tput sgr0 2>/dev/null)"
 BLACK="\033[0;30m"
@@ -51,8 +129,6 @@ ORANGE="\033[0;33m"
 LIGHTRED='\033[1;31m'
 BG_GREEN="\[$(tput setab 2 2>/dev/null)\]"
 BG_RED="\[$(tput setab 9 2>/dev/null)\]"
-exitCode=
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Application Folders
 
@@ -63,7 +139,7 @@ exitCode=
 # Set options
 SETARGS="$*"
 SHORTOPTS=""
-LONGOPTS="options,version,help,raw"
+LONGOPTS="version,help,raw"
 ARRAY=""
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Setup application options
@@ -80,14 +156,6 @@ while :; do
     shift 1
     printf_color "$YELLOW" "$APPNAME Version: $VERSION\n"
     exit
-    ;;
-  --options)
-    shift 1
-    [ -n "$1" ] || printf_color "$PURPLE" "Current options for ${PROG:-$APPNAME}\n"
-    [ -z "$SHORTOPTS" ] || __list_options "Short Options" "-$SHORTOPTS" ',' '-'
-    [ -z "$LONGOPTS" ] || __list_options "Long Options" "--$LONGOPTS" ',' '--'
-    [ -z "$ARRAY" ] || __list_options "Base Options" "$ARRAY" ',' ''
-    exit $?
     ;;
   --raw)
     shift 1
@@ -113,33 +181,57 @@ else
   BUILD_DESTDIR="${BUILD_DESTDIR:-/usr/local}"
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Main application
-if [[ -d "$BUILD_SRC_DIR" ]]; then
-  builtin cd "$BUILD_SRC_DIR" || exit 1
-  if [[ -d ".git" ]]; then
-    git reset --hard &>/dev/null && git pull -q &>/dev/null
-  elif [[ -n "$BUILD_SRC_URL" ]]; then
-    if ! git clone "$BUILD_SRC_URL" "$BUILD_SRC_DIR"; then
-      printf_color "1" "Failed to clone from $BUILD_SRC_URL"
-      exit 1
-    fi
+# Install required packages
+if cmd_exists apt-get; then
+  if cmd_exists pkmgr; then
+    for pkg in libxml2-dev libmenu-cache-dev lxmenu-data libpango1.0-dev librsvg2-dev libcairo2-dev libxrandr-dev build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev; do
+      printf_color "$CYAN" "Installing $pkg"
+      pkmgr silent install $pkg &>/dev/null
+      [[ $? = 0 ]] && printf_color "$GREEN" "Installed $pkg" || printf_color "$RED" "Failed to installed $pkg"
+    done
   fi
-  if [[ -f "configure" ]]; then
-    ./configure --prefix="$BUILD_DESTDIR" --with-lx --with-pmenu
-    sudo make clean install DESTDIR="$BUILD_DESTDIR" 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+elif cmd_exists dnf; then
+  if cmd_exists pkmgr; then
+    for pkg in cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel; do
+      printf_color "$CYAN" "Installing $pkg"
+      pkmgr silent install $pkg &>/dev/null
+      [[ $? = 0 ]] && printf_color "$GREEN" "Installed $pkg" || printf_color "$RED" "Failed to installed $pkg"
+    done
   fi
-  if [[ -f "$BUILD_LOG_FILE" ]]; then
-    errors="$(grep 'fatal error' "$BUILD_LOG_FILE" || echo '')"
-    if [[ -n "$errors" ]]; then
-      printf_color "$RED" "The following errors have occurred:"
-      echo -e "$errors"
-    else
-      rm -Rf "$BUILD_LOG_FILE" &>/dev/null
-      printf_color "$GREEN" "Build of $BUILD_NAME has completed without error"
-    fi
+elif cmd_exists yum; then
+  if cmd_exists pkmgr; then
+    for pkg in cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel; do
+      printf_color "$CYAN" "Installing $pkg"
+      pkmgr silent install $pkg &>/dev/null
+      [[ $? = 0 ]] && printf_color "$GREEN" "Installed $pkg" || printf_color "$RED" "Failed to installed $pkg"
+    done
   fi
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main application
+if [[ -d "$BUILD_SRC_DIR" ]]; then
+  if ! builtin cd "$BUILD_SRC_DIR"; then
+    printf_color "$RED" "Failed to cd into $BUILD_SRC_DIR"
+    exit 1
+  fi
+  __run_git
+  __make_build
+  __check_log
+  exitCode=$?
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check
+if [[ "$exitCode" -eq 0 ]] && builtin type -P "$BUILD_NAME"; then
+  printf_color "$GREEN" "Successfully installed $BUILD_NAME"
+  exitCode=0
+else
+  printf_color "$RED" "Failed to install $BUILD_NAME"
+  exitCode=1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End application
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
 exit ${exitCode:-$?}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
